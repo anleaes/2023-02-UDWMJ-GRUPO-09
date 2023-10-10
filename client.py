b6e144f500eded7d1de1f330807470be8f8511f5
GabrielYanzerCarlesso <GabrielYanzerCarlesso@gmail.com>
2023-10-10 13:30:23 -0300

    implementando classe network e orderitem

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-class client:
+class Client:
     def __init__(self,first_name,last_name,address,ceH_phone,email,gender):
         self.first_name = first_name
         self.last_name = last_name
---
+++ orderitem.py
@@ -0,0 +1,5 @@
+class Orderitem:
+    def __init__(self,quantity,unitary_price,order,product):
+        self.quantity = quantity
+        self.unitary_price = unitary_price
+        self.order,order = order,order
