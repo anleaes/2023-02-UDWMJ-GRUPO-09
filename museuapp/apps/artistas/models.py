23bebf98d76d526a4031ee3af6fc456c93a603d0
Diogo288 <diogobueno443@gmail.com>
2023-12-04 11:43:15 -0300

    Arrumando-erros

--- museuapp/apps/artistas/models.py
+++ museuapp/apps/artistas/models.py
@@ -6,7 +6,7 @@ class Artista(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     name = models.CharField('Nome', max_length=50)
-    description = models.TextField('Descricao', max_length=100) 
+    description = models.CharField('Descricao', max_length=100) 
     
     class Meta:
         verbose_name = 'artistas'
